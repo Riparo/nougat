66526e0c2b0b7768a6e62c7c3c3b7f6690ce8dfc
Kilerd Chan <blove694@gmail.com>
2017-04-20 22:25:49 +0800

    base middleware model

---
+++ misuzu/middleware.py
@@ -0,0 +1,13 @@
+class BaseMiddleware:
+    """
+    the basic middleware model
+    """
+
+    def __init__(self):
+        pass
+
+    def on_request(self, request):
+        pass
+
+    def on_response(self, response):
+        pass
