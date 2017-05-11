2744e1bee554cd9b1fcb50528d2e15845d946f8f
Kilerd Chan <blove694@gmail.com>
2017-05-12 00:14:04 +0800

    feature: add is_middleware function

--- misuzu/utils.py
+++ misuzu/utils.py
@@ -1,8 +1,22 @@
+import inspect
+from misuzu.exceptions import UnknownMiddlewareException
 
-def is_middleware(one):
+
+def is_middleware(func):
     """
     test whether it is a middleware
     :return: Boolean
     """
-    # TODO is middleware function
-    pass
+    args = list(inspect.signature(func).parameters.items())
+
+    if not inspect.iscoroutinefunction(func):
+        raise UnknownMiddlewareException("middleware {} should be awaitable".format(func.__name__))
+
+    if len(args) != 2:
+        raise UnknownMiddlewareException("middleware {} should has 2 params named ctx and next".format(func.__name__))
+
+    if args[0][0] != 'ctx':
+        raise UnknownMiddlewareException("the first param's name of middleware {} should be ctx".format(func.__name__))
+
+    if args[1][0] != 'next':
+        raise UnknownMiddlewareException("the second param's name of middleware {} should be next".format(func.__name__))
