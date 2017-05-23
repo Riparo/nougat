35073a4e452a307b5b0fcbc095bdf05a553c721a
Kilerd Chan <blove694@gmail.com>
2017-05-23 23:36:58 +0800

    test: some test case of section  - repeat section name  - redefine url in one section  - redefine url in diff section  - redefine handler name in same section

--- tests/test_section.py
+++ tests/test_section.py
@@ -1,12 +1,67 @@
+import pytest
+from misuzu import Misuzu, Section
+from misuzu import HandlerRedefineException, MisuzuRuntimeError, RouteReDefineException
+
 
 def test_section():
     pass
 
+
 def test_repeat_section_name():
-    pass
+    with pytest.raises(MisuzuRuntimeError):
+        app = Misuzu()
+        a = Section("a")
+        b = Section("a")
+
+        app.use(a)
+        app.use(b)
+
 
 def test_in_same_section_redefine_url():
-    pass
+    with pytest.raises(RouteReDefineException):
+        app = Misuzu()
+        section = Section("section")
+
+        @section.get("/")
+        async def index(ctx):
+            pass
+
+        @section.get("/")
+        async def indexs(ctx):
+            pass
+
+        app.use(section)
+
 
 def test_in_diff_section_redefine_url():
-    pass
+    with pytest.raises(RouteReDefineException):
+        app = Misuzu()
+        a = Section("a")
+        b = Section("b")
+
+        @a.get("/")
+        async def index(ctx):
+            pass
+
+        @b.get("/")
+        async def indexs(ctx):
+            pass
+
+        app.use(a)
+        app.use(b)
+
+
+def test_in_same_section_redefine_handler():
+    with pytest.raises(HandlerRedefineException):
+        app = Misuzu()
+        section = Section("section")
+
+        @section.get("/")
+        async def index(ctx):
+            pass
+
+        @section.get("/a")
+        async def index(ctx):
+            pass
+
+        app.use(section)
