22ff93341adc9dd890820b603990b88a7cae37e5
Kilerd Chan <blove694@gmail.com>
2017-10-24 23:41:40 +0800

    feature: add http base method to nougat index for easily importing

--- nougat/__init__.py
+++ nougat/__init__.py
@@ -5,5 +5,6 @@ from nougat.protocol import *
 from nougat.router import *
 from nougat.section import *
 from nougat.test_client import *
+from nougat.routing import Routing, get, post, put, delete, patch
 
 __version__ = "0.1.1a1"
